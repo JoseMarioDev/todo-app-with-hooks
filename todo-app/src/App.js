aadd8957354816d4312a5d08438554477211bcb8
josemariodev <josemariodev@gmail.com>
2020-03-27 15:55:09 -0500

    eslintrc added

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
-  return <div>hello world</div>;
-}
+const App = () => {
+  const [name, setName] = useState();
+
+  let x;
+
+  return (
+    <div>
+      <form>
+        <input
+          type='text'
+          value={name}
+          placeholder='enter a name'
+          onChange={event => {
+            setName(event.target.value);
+          }}
+        ></input>
+      </form>
+      <h4>{name}</h4>
+    </div>
+  );
+};
 
 export default App;
