80fe6490523765a103e9cd409bd36645e6aeef6f
josemariodev <josemariodev@gmail.com>
2020-03-28 07:34:28 -0500

    :lipstick: Installed styled components

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,24 +1,44 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  /* outline: 1px solid red; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* margin: 10% auto; */
+  padding: 40px;
+`;
+const Form = styled.form`
+  margin: 0 auto;
+`;
+const H4 = styled.h4`
+  font-size: 20px;
+  color: navy;
+`;
+const Input = styled.input`
+  padding: 5px 10px;
+  width: 200px;
+`;
 
 const App = () => {
   const [name, setName] = useState();
 
-  let z;
-
   return (
-    <div>
-      <form>
-        <input
+    <Container>
+      <Form>
+        <Input
           type="text"
           value={name}
           placeholder="enter a name"
           onChange={event => {
             setName(event.target.value);
           }}
-        ></input>
-      </form>
-      <h4>{name}</h4>
-    </div>
+        ></Input>
+      </Form>
+      <H4>my name is: {name}</H4>
+    </Container>
   );
 };
 
